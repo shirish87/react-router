ac1fe57a0491ac712d78b04a99e785a9674ffcf4
Shirish Kamath <shirishk.87@gmail.com>
2016-04-19 20:16:04 +0530

    Reduced test browsers

--- karma.conf.js
+++ karma.conf.js
@@ -8,40 +8,12 @@ module.exports = function (config) {
 
   var customLaunchers = {
     // Browsers to run on BrowserStack.
-    BS_Chrome: {
-      base: 'BrowserStack',
-      os: 'Windows',
-      os_version: '10',
-      browser: 'chrome',
-      browser_version: '47.0'
-    },
     BS_Firefox: {
       base: 'BrowserStack',
       os: 'Windows',
       os_version: '10',
       browser: 'firefox',
       browser_version: '43.0'
-    },
-    BS_Safari: {
-      base: 'BrowserStack',
-      os: 'OS X',
-      os_version: 'El Capitan',
-      browser: 'safari',
-      browser_version: '9.0'
-    },
-    BS_MobileSafari: {
-      base: 'BrowserStack',
-      os: 'ios',
-      os_version: '8.3',
-      browser: 'iphone',
-      real_mobile: false
-    },
-    BS_MobileSafari: {
-      base: 'BrowserStack',
-      os: 'ios',
-      os_version: '9.1',
-      browser: 'iphone',
-      real_mobile: false
     },
     BS_InternetExplorer10: {
       base: 'BrowserStack',
